b1faceb7441ea497562443ca3832b465c84856c7
Prajwal_Y_P <4mh19cs071@gmail.com>
2022-08-26 21:16:30 +0530

    char

---
+++ char.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    char a{97};
+    char A{65};
+
+    std::cout<<"\na\t: "<<a<<"\nA\t: "<<A<<std::endl;
+
+    char n;
+
+    std::cin>>n;
+    std::cout<<std::endl<< n;
+
+    return 0;
+}
