535e1e60159e8f9f8ed8a6095bca85858e73cb2b
Prajwal_Y_P <4mh19cs071@gmail.com>
2022-08-30 12:47:28 +0530

    eleven

---
+++ functionOverloading/overload.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+void max(const std::string& s1,const std::string& s2,std::string& s3)
+{
+    if(s1>s2)
+        s3=s1;
+    else
+        s3=s2;
+}
+
+void max(int s1,int s2,int& s3)
+{
+    if(s1>s2)
+        s3=s1;
+    else
+        s3=s2;
+}
+
+void max(double s1,double s2,double* s3)
+{
+    if(s1>s2)
+        *s3=s1;
+    else
+        *s3=s2;
+}
+
+int main()
+{
+    std::string s1("YP");
+    std::string s2("UJJWAL");
+    std::string s3;
+
+    max(s1,s2,s3);
+    std::cout<<"max : "<<s3<<std::endl;
+
+    int a{11},b{45},c{};
+    max(a,b,c);
+    std::cout<<"maxi : "<<c<<std::endl;
+
+    double a1{611.8},b1{456.8},c1{};
+    max(a1,b1,&c1);
+    std::cout<<"maxd : "<<c1<<std::endl;
+
+
+    return 0;    
+}
